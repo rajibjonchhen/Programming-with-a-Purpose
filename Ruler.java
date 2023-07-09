324da63f3203fa21e7ec2e8994401d5bda891be8
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-07-09 12:03:22 +0100

    ruler app

---
+++ Ruler.java
@@ -0,0 +1,11 @@
+public class Ruler {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        String ruler = ""; 
+
+        for(int i = 1; i <= n; i++) {
+            ruler = ruler + i + ruler;
+            System.out.println(ruler);
+        }
+    }
+}
